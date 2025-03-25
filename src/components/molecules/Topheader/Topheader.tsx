90d110f5116290733401018fed754fe3b85e3689
Brayan Yepes <abrayan.claye@gmail.com>
2025-03-25 17:27:14 -0500

    feat: add test in selector of idiom

--- src/components/molecules/Topheader/Topheader.tsx
+++ src/components/molecules/Topheader/Topheader.tsx
@@ -1,13 +1,14 @@
+"use client";
+
 import { LucideArrowRight } from "lucide-react";
+import { useTranslations } from "next-intl";
 
 export const Topheader = () => {
+  const t = useTranslations();
   return (
     <section className="hidden md:flex justify-center items-center py-3 bg-blue">
       <div className="inline-flex gap-2 items-center">
-        <p className="text-background/70 text-sm">
-          Consulta gratuita y sin compromiso. Accede a las mejores opciones de
-          seguros de manera rápida y sencilla.
-        </p>
+        <p className="text-background/70 text-sm">{t("topHeader.message")}</p>
         <LucideArrowRight className="text-white inline-flex justify-center items-center w-5 h-5" />
       </div>
     </section>
